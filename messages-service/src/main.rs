4e29b402d805d6e8ad36101ab2c5831654dcc3de
Nazar Demchuk <nazar.demchuk@lynxtrading.com>
2024-02-06 23:18:34 +0200

    Everything works

--- messages-service/src/main.rs
+++ messages-service/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{get, launch, routes};
+
+#[get("/message")]
+fn message() -> &'static str {
+    "not implemented yet"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .configure(rocket::Config::figment().merge(("port", 8002)))
+        .mount("/", routes![message])
 }
